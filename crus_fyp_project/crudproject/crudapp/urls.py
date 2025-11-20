559d53d6f6f0b94733e9e1de737f95f71ec4c14c
Rao Mudassar Ali <engjames.usa@gmail.com>
2025-11-20 22:27:08 +0500

    Add files via upload

---
+++ crus_fyp_project/crudproject/crudapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.showView, name='home'),  # Add root URL pattern
+    path('ofv/', views.orderFormView, name='order_url'),
+    path('sv/', views.showView, name='show_url'),
+    path('up/<int:f_oid>', views.updateView, name= 'update_url'),
+    path('del/<int:f_oid>', views.deleteView, name= 'delete_url'),
+]
